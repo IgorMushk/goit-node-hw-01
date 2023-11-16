2c0c5081c5a081ac9569bf417d25b4a281a84685
IgorMushk <igor.mushketik@gmail.com>
2023-11-16 17:47:24 +0200

    del comments lines

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const {
     
     const argv = program.opts();
     
-    // TODO: рефакторить
     async function invokeAction({ action, id, name, email, phone }) {
       switch (action) {
         case 'list':
@@ -47,34 +46,3 @@ const {
     }
     
     invokeAction(argv);
-
-// test
-// async function testListContacts() {
-//   const list = await listContacts();
-//   console.log(list);
-// }
-  
-// testListContacts();
-
-//   async function testGetContactById() {
-//   const list = await getContactById('05olLMgyVQdWRwgKfg5J6');
-//   console.log(list);
-// }
-  
-// testGetContactById();
-
-//{ "name": "Vasyl", "email": "Vasyl@magnis.org","phone": "(000) 123-4567"}
-//  async function testAddContact() {
-//   const list = await addContact("Vasyl", "Vasyl@magnis.org","(000) 123-4567");
-//   console.log(list);
-// }
-  
-// testAddContact();
-
-//JsGjsm4ejx55R-Ne5ZVip
-//  async function testRemoveContact() {
-//   const list = await removeContact("JsGjsm4ejx55R-Ne5ZVip");
-//   console.log(list);
-// }
-  
-// testRemoveContact();
