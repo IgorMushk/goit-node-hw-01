5357321bbc6492cdc60e81dc5ac8dc70313ba4df
IgorMushk <igor.mushketik@gmail.com>
2023-11-16 15:47:10 +0200

    upd - test addContac

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const {nanoid} = require("nanoid");
 
 const contactsPath = path.join(__dirname,"/db/contacts.json");
 console.log(contactsPath);
@@ -20,8 +21,17 @@ async function listContacts() {
     // если объект с таким id не найден.
   }
   
-  function addContact(name, email, phone) {
-    // ...твой код. Возвращает объект добавленного контакта. 
+  async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {
+     id:  nanoid(),
+     name,
+     email,
+     phone
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath,JSON.stringify(contacts,null,2));
+    return newContact;
   };
 
 // test
@@ -32,9 +42,17 @@ async function listContacts() {
   
 // testListContacts();
 
- async function testGetContactById() {
-  const list = await getContactById('05olLMgyVQdWRwgKfg5J6');
+//  async function testGetContactById() {
+//   const list = await getContactById('05olLMgyVQdWRwgKfg5J6');
+//   console.log(list);
+// }
+  
+// testGetContactById();
+
+//{ "name": "Vasyl", "email": "Vasyl@magnis.org","phone": "(000) 123-4567"}
+ async function testAddContact() {
+  const list = await addContact("Vasyl", "Vasyl@magnis.org","(000) 123-4567");
   console.log(list);
 }
   
-testGetContactById();
+testAddContact();
